5018e926882055e2c2cdf075248ced22cc427b99
Mohit Kumar <mohit.6.kumar.ext@nokia.com>
2021-07-19 15:32:39 +0530

    Upload

---
+++ file.js
@@ -0,0 +1,16 @@
+function whatsappApi(){
+    var phoneNumber = document.getElementById("phone").value;
+    if(phoneNumber.length<10 || phoneNumber.length>13){
+       document.getElementById("msg").innerText="*Please enter valid 10 digit mobile number";
+    }
+    else{
+        if (phoneNumber != null || phoneNumber!=undefined) 
+        { 
+            window.location.href = "https://api.whatsapp.com/send?phone=" + phoneNumber; 
+        }
+        else{
+            document.getElementById("msg").innerText="Something went wrong. Please try again later";
+        }
+    }
+    
+}
