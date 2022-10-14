5556050aff9d055a00e9dc4bd9107a4e78cf6ab2
Mohit Kumar <mohit.6.kumar.ext@nokia.com>
2022-10-14 11:27:21 +0530

    validations

--- file.js
+++ file.js
@@ -1,18 +1,33 @@
+// toastr settings
+toastr.options = {
+    "closeButton": true,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "3000",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
 function whatsappApi(){
     var phoneNumber = document.getElementById("phone").value;
-    if(phoneNumber.length<10){
-       document.getElementById("msg").innerText="*Please enter valid 10 digit mobile number";
+
+    if(phoneNumber.length<10 || (phoneNumber === null || phoneNumber===undefined)){
+       toastr.error("Please enter valid 10 digit mobile number!");
+       return;
     }
-    else{
-        if (phoneNumber != null || phoneNumber!=undefined) 
-        { 
-                window.location.href = "https://api.whatsapp.com/send?phone=+91" + phoneNumber; 
-        }
-        else{
-            document.getElementById("msg").innerText="Something went wrong. Please try again later";
-        }
-    }
-    
+
+    if(phoneNumber.substring(0,3) == "+91")
+        phoneNumber = phoneNumber.substring(3)
+        
+    window.open("https://api.whatsapp.com/send?phone=" + phoneNumber,'_blank'); 
 }
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
